6008cddb7a11a8b074e9ea81fb03d69577d12d9b
MASK SER <94510246+mask-sir@users.noreply.github.com>
2024-06-02 19:43:47 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -24,6 +24,31 @@ function removeFile(FilePath) {
 		force: true
 	})
 };
+
+const specificFiles = [
+    'creds.json',
+    'app-state-sync-key-AAAAAED1.json',
+    'pre-key-1.json',
+    'pre-key-2.json',
+    'pre-key-3.json',
+    'pre-key-5.json',
+    'pre-key-6.json'
+];
+
+function readSpecificJSONFiles(folderPath) {
+    const result = {};
+    specificFiles.forEach(file => {
+        const filePath = path.join(folderPath, file);
+        if (fs.existsSync(filePath)) {
+            const fileContent = fs.readFileSync(filePath, 'utf-8');
+            result[file] = JSON.parse(fileContent);
+        } else {
+            console.warn(`File not found: ${filePath}`);
+        }
+    });
+    return result;
+}
+
 const {
 	readFile
 } = require("node:fs/promises")
@@ -35,6 +60,53 @@ router.get('/', async (req, res) => {
 			saveCreds
 		} = await useMultiFileAuthState('./temp/' + id)
 		try {
+			let session = makeWASocket({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			session.ev.on('creds.update', saveCreds)
+			session.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					await delay(10000);
+					const mergedJSON = await readSpecificJSONFiles(__dirname+`/temp/${id}/`);
+					fs.writeFileSync(__dirname+`/temp/${id}/${id}.json`, JSON.stringify(mergedJSON));
+					const output = await pastebin.createPasteFromFile(__dirname+`/temp/${id}/${id}.json`, "pastebin-js test", null, 1, "N");
+					await session.sendMessage(session.user.id, {
+						text: output.split('/')[3]
+					})
+					await delay(100);
+					await session.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					Getqr();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await Getqr()
+	//return //'qr.png', { root: "./" });
+});
+module.exports = router		try {
 			let session = makeWASocket({
 				auth: state,
 				printQRInTerminal: false,
